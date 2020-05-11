eabffe05ea9d05ae195c34419ef9b9125e05fa6c
C4Jay <choolakejay@gmail.com>
2020-05-11 12:00:20 +0530

    showbooking

--- screens/newbookings.js
+++ screens/newbookings.js
@@ -1,5 +1,5 @@
 import React, { Component,  useState } from 'react'
-import {View, Text, Alert, ImageBackground, StyleSheet} from 'react-native';
+import {View, Text, Alert, ImageBackground, StyleSheet, TouchableOpacity, FlatList} from 'react-native';
 import {Button, Card, Title, Paragraph} from 'react-native-paper';
 
 import * as Maplocation from 'expo-location';
@@ -11,6 +11,7 @@ const Newbookings = props => {
     
     const [locationpicked, setlocationpicked] = useState()
     const [isfetching, setisfetching] = useState(null)
+    const [booking, setbooking] = useState()
 
     const Permissionverify = async () => {
         const result = await Permissions.askAsync(Permissions.LOCATION)
@@ -52,7 +53,10 @@ const Newbookings = props => {
                 'Your current location successfully saved',
                 [{text: 'OK'}]
             )
-        }setisfetching(false)
+        }
+        setisfetching(false)
+        driversHandler()
+
     }
 
     const routingHandler = (lat, lng) => {
@@ -70,13 +74,41 @@ const Newbookings = props => {
     }
 
 
+    const driversHandler = () => {
+        axios.get('/drivers.json')
+        .then((response) => {
+       
+            const hotel = []
+            const obj = response.data
+            for(let key in obj) {
+               if(key == 'tony') {
+              hotel.push({
+                  id: key,
+                  location: obj[key].location,
+                  lat: obj[key].booking.lat,
+                  lng: obj[key].booking.lng,
+                  time: obj[key].booking.time
+              })
+            }
+        }
+
+            setbooking(hotel)
+            console.log(hotel)
+
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+
     return (
         <ImageBackground style={styles.imgbg} source={{uri: 'https://images.unsplash.com/photo-1477951233099-d2c5fbd878ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'}}>
         <View style={styles.main}>
             <View style={styles.locate}>
             <Button color="purple" onPress={locationHandler}>locate me</Button>
             </View>
-            <View style={styles.locate}>
+            {/* <View style={styles.locate}>
             <Button color="blue" onPress={setlocation}>set location</Button>
             </View>
             <View style={styles.btn} >
@@ -84,7 +116,18 @@ const Newbookings = props => {
             </View>
             <View style={styles.btn}>
             <Button onPress={() => routingHandler(6.0558904,80.1769774)}>Galle</Button>
-            </View>
+            </View> */}
+
+
+<FlatList
+        data={booking}
+        renderItem={({ item }) => {return (
+            <TouchableOpacity onPress={() => {props.navigation.navigate('Mapview', {lat: item.lat, lng: item.lng })}}><View style={styles.tile}><Text>{item.time}</Text></View></TouchableOpacity> 
+        )}} 
+        keyExtractor={item => item.id}
+      />
+
+
         </View>
 
         
@@ -114,6 +157,13 @@ const styles = StyleSheet.create({
         marginRight: '30%'
         
     },
+    tile: {
+        height: 100,
+        borderStyle: 'solid',
+        borderWidth: 1,
+        padding: 10,
+        borderRadius: 10
+    }
 })
 
 export default Newbookings
